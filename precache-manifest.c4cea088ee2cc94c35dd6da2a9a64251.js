cf3380acdaf5701961faae704d1b8ba6748d2677
Meow Merry <meow3525@gmail.com>
2020-07-22 13:00:05 +0700

    Updates

---
+++ precache-manifest.c4cea088ee2cc94c35dd6da2a9a64251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19330add5dce5843bcdb42a3119c86c4",
+    "url": "/shibashop/index.html"
+  },
+  {
+    "revision": "df32f3d6255b7a7241bb",
+    "url": "/shibashop/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "611ab7ef9b545f351b4d",
+    "url": "/shibashop/static/css/main.3c3ab016.chunk.css"
+  },
+  {
+    "revision": "df32f3d6255b7a7241bb",
+    "url": "/shibashop/static/js/2.cf9cb27a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shibashop/static/js/2.cf9cb27a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611ab7ef9b545f351b4d",
+    "url": "/shibashop/static/js/main.723df3da.chunk.js"
+  },
+  {
+    "revision": "f2792be7ff2dc2285b20",
+    "url": "/shibashop/static/js/runtime-main.492f35f6.js"
+  }
+]);
